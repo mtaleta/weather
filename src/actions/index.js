65bafa83ad4249ffd471264f3feb23604e225c21
mtal <ex051240@gmail.com>
2017-11-21 21:36:35 +0800

    updata

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const API_KEY = '2c9d894f6971e96d756c4a3029cc6e3d';
-const ROOT_URL=`http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL=`http://api.openweathermap.org/data/2.5/forecast?`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
-    const url = `${ROOT_URL}&q=${city},us`;
+    const url = `${ROOT_URL}q=${city},us&appid=${API_KEY}`;
     const request = axios.get(url);
 
 
